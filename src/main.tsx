a7391e30cdde87d6e2d9af23dd1174780e0ba64b
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-08-12 15:05:46 +0200

    Create redux store.

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,22 @@ import './styles/index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
 import Layout from './components/Layout/Layout.tsx'
 import ToastifyWrapper from './components/ToastifyWrapper/ToastifyWrapper.tsx'
+import { store } from './store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Layout>
-				<App />
-				<ToastifyWrapper />
+				<Provider store={store}>
+					<App />
+					<ToastifyWrapper />
+				</Provider>
 			</Layout>
 		</BrowserRouter>
 	</React.StrictMode>
