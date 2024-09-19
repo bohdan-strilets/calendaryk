d277548f01773f9137b2d297ca60994f1fd7cad0
BS <bohdan.strilets@gmail.com>
2024-09-19 19:36:30 +0200

    Created gallery for user avatar.

--- src/components/Profile/AllAvatars/AllAvatars.styled.ts
+++ src/components/Profile/AllAvatars/AllAvatars.styled.ts
@@ -1,4 +1,7 @@
 import styled from '@emotion/styled'
+import { motion } from 'framer-motion'
+
+import { screenWidth } from '@/utils/data/screenWidth'
 
 export const List = styled.ul`
 	display: flex;
@@ -9,19 +12,65 @@ export const List = styled.ul`
 	padding: 0;
 `
 
-export const Item = styled.li`
-	flex: 1 1 calc(33% - 5px);
+export const Item = styled(motion.li)`
+	flex: 1 1 100%;
 	box-sizing: border-box;
-	max-width: calc(33% - 5px);
+	max-width: 100%;
+	height: 200px;
+
+	@media screen and (min-width: ${screenWidth.TABLET}) {
+		flex: 1 1 calc(33% - 5px);
+		max-width: calc(33% - 5px);
+	}
 `
 
 export const Image = styled.div<{ url: string }>`
 	width: 100%;
-	height: 200px;
+	height: 100%;
 
 	background-color: var(--gray-color);
 	background-image: ${({ url }) => `url(${url})`};
 	background-position: center;
 	background-repeat: no-repeat;
 	background-size: cover;
+
+	cursor: pointer;
+	transition: transform var(--hover-effect);
+
+	:hover,
+	:focus {
+		transform: scale(1.02);
+	}
+`
+
+export const PreviewWrapper = styled(motion.div)`
+	position: relative;
+`
+
+export const CollapseButton = styled.button`
+	position: absolute;
+	top: 15px;
+	right: 15px;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	width: 40px;
+	height: 40px;
+
+	background-color: var(--gray-color);
+	color: var(--white-color);
+
+	cursor: pointer;
+	transition: background-color var(--hover-effect);
+
+	:hover,
+	:focus {
+		background-color: var(--green-color);
+	}
+
+	:active {
+		transform: scale(0.95);
+	}
 `
--- src/components/Profile/AllAvatars/AllAvatars.tsx
+++ src/components/Profile/AllAvatars/AllAvatars.tsx
@@ -1,21 +1,57 @@
-import { FC } from 'react'
+import { AnimatePresence } from 'framer-motion'
+import { FC, useState } from 'react'
+import { BiCollapse } from 'react-icons/bi'
 
+import Avatar from '@/components/UI/Avatar'
 import { useAppSelector } from '@/hooks/useAppSelector'
 import { getAvatars } from '@/store/user/userSelectors'
 
-import { Image, Item, List } from './AllAvatars.styled'
+import {
+	CollapseButton,
+	Image,
+	Item,
+	List,
+	PreviewWrapper,
+} from './AllAvatars.styled'
 
 const AllAvatars: FC = () => {
+	const [selectedImage, setSelectedImage] = useState<null | string>(null)
+
 	const allAvatars = useAppSelector(getAvatars)
 
-	return (
+	const selectImage = (url: string) => setSelectedImage(url)
+	const clearSelectedImage = () => setSelectedImage(null)
+
+	return !selectedImage ? (
 		<List>
-			{allAvatars?.map((url) => (
-				<Item key={url}>
+			{allAvatars?.map((url, index) => (
+				<Item
+					key={url}
+					onClick={() => selectImage(url)}
+					initial={{ y: '-100%', opacity: 0 }}
+					animate={{ y: '0%', opacity: 1 }}
+					transition={{ duration: 0.2, delay: index * 0.1 }}
+				>
 					<Image url={url} />
 				</Item>
 			))}
 		</List>
+	) : (
+		<AnimatePresence>
+			{selectedImage && (
+				<PreviewWrapper
+					initial={{ opacity: 0, scale: 0.8, y: '100%' }}
+					animate={{ opacity: 1, scale: 1, y: 0 }}
+					exit={{ opacity: 0, scale: 0.8, y: '100%' }}
+					transition={{ duration: 0.3, ease: 'easeInOut' }}
+				>
+					<CollapseButton type="button" onClick={clearSelectedImage}>
+						<BiCollapse size={24} />
+					</CollapseButton>
+					<Avatar imageUrl={selectedImage ?? ''} height="480px" width="100%" />
+				</PreviewWrapper>
+			)}
+		</AnimatePresence>
 	)
 }
 
