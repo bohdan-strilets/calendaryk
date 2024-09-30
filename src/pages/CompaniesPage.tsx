a10fbf5f43bdb6da44b4bdf740ed739bd6e5ab6f
BS <bohdan.strilets@gmail.com>
2024-09-30 19:26:02 +0200

    Corrected company page.

--- src/pages/CompaniesPage.tsx
+++ src/pages/CompaniesPage.tsx
@@ -8,12 +8,15 @@ import Statistics from '@/components/ListCompanies/Statistics'
 import Modal from '@/components/Modal'
 import Button from '@/components/UI/Button'
 import Loader from '@/components/UI/Loader'
+import { useAppSelector } from '@/hooks/useAppSelector'
 import useModal from '@/hooks/useModal'
 import useResponsive from '@/hooks/useResponsive'
+import { getIsLoggedIn } from '@/store/auth/authSelectors'
 import { useGetAllQuery } from '@/store/companies/companyApi'
 
 const CompaniesPage: FC = () => {
 	const { checkQueryParam, modalNames, openModal } = useModal()
+	const isLoggedIn = useAppSelector(getIsLoggedIn)
 	const { isMaxTablet } = useResponsive()
 	const { data, isLoading } = useGetAllQuery()
 	const companies = data?.data
@@ -22,15 +25,17 @@ const CompaniesPage: FC = () => {
 		<Loader />
 	) : (
 		<>
-			<Button
-				type="button"
-				onClick={() => openModal(modalNames.NEW_COMPANY)}
-				height={isMaxTablet ? '80' : '140px'}
-				variant="black"
-				margin={isMaxTablet ? '0 0 20px 0' : '0 0 40px 0'}
-			>
-				<TbLayoutGridAdd size={isMaxTablet ? 55 : 80} />
-			</Button>
+			{isLoggedIn && (
+				<Button
+					type="button"
+					onClick={() => openModal(modalNames.NEW_COMPANY)}
+					height={isMaxTablet ? '80' : '140px'}
+					variant="black"
+					margin={isMaxTablet ? '0 0 20px 0' : '0 0 40px 0'}
+				>
+					<TbLayoutGridAdd size={isMaxTablet ? 55 : 80} />
+				</Button>
+			)}
 			<ListCompanies companies={companies} />
 			<Statistics companies={companies} />
 
